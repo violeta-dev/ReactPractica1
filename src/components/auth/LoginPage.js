0127d6f5685e9e6646e110d58d10b241e50d1da2
David Jiménez <davidjj76@gmail.com>
2020-11-12 22:24:43 +0100

    Manage inputs in login form

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -6,18 +6,52 @@ import Button from '../shared/Button';
 import './LoginPage.css';
 
 class LoginPage extends React.Component {
+  state = {
+    form: {
+      email: '',
+      password: '',
+    },
+  };
+
+  handleChange = event => {
+    this.setState(state => ({
+      form: { ...state.form, [event.target.name]: event.target.value },
+    }));
+  };
+
+  canSubmit = () => {
+    const {
+      form: { email, password },
+    } = this.state;
+    return email && password;
+  };
+
   render() {
+    const {
+      form: { email, password },
+    } = this.state;
+
     return (
       <div className="loginPage">
         <h1 className="loginPage-title">Log in to Twitter</h1>
         <form>
-          <input type="text" />
-          <input type="password" />
+          <input
+            name="email"
+            type="text"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={this.handleChange}
+          />
           <Button
             type="submit"
             className="loginPage-submit"
             variant="primary"
-            // disabled
+            disabled={!this.canSubmit()}
           >
             Log in
           </Button>
